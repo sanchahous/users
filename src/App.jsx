1a576568acad99d2ceed85a43cedbef11a14dad4
sanchahous <sanchahous@gmail.com>
2020-12-30 18:09:39 +0200

    Add stripe card validation logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ import {Route, Redirect, Switch, withRouter} from 'react-router-dom';
 import {UsersList} from "./components/Users/UsersList/UsersList";
 import {CreateUser} from "./components/Users/CreateUser/CreateUser";
 import {UpdateUser} from "./components/Users/UpdateUser/UpdateUser";
+import {CheckoutForm} from "./components/Stripe/Stripe";
+
+import {loadStripe} from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+import {
+  Elements
+} from '@stripe/react-stripe-js';
 
 class App extends React.Component {
+
   render() {
     return (
         <React.Fragment>
@@ -31,6 +40,13 @@ class App extends React.Component {
                 path="/users/update/:id"
                 component={UpdateUser}
             />
+            <Route
+                path="/stripe"
+            >
+              <Elements stripe={stripePromise}>
+                <CheckoutForm />
+              </Elements>
+            </Route>
           </Switch>
 
         </React.Fragment>
